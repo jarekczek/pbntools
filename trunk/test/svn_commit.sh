5fdf3e00ce9b5348827cfc7c9a1ca03230e3a09b
jarekczek <jarekczek@cba49596-2f5b-4b15-a11e-bdda880d9e08>
2011-10-31 18:38:46 +0000

    svn_commit script: configurable username

--- trunk/test/svn_commit.sh
+++ trunk/test/svn_commit.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-# rm -rf /tmp/pbncode
-# mkdir /tmp/pbncode &&
-#svn checkout --username=jarekczek svn+ssh://jarekczek@svn.code.sf.net/p/pbntools/code/trunk /tmp/pbncode &&
 pbndir=`dirname $0`/..
+rm -rf /tmp/pbncode
+mkdir /tmp/pbncode &&
+svnUser=`sed -n 's/\./_/g; s/^svn_user=\(.*\)$/\1/p' $pbndir/local.properties` &&
+# svnPass=`sed -n 's/\./_/g; s/^svn_pass=\(.*\)$/\1/p' $pbndir/local.properties` &&
+svn checkout --non-interactive --username $svnUser svn+ssh://${svnUser}@svn.code.sf.net/p/pbntools/code/trunk /tmp/pbncode &&
 version=`sed -n 's/\./_/g; s/^wersja=\(.*\)$/\1/p' $pbndir/src/jc/pbntools/PbnTools.properties` &&
 rev=`sed -n 's/^build.number=\(.*\)$/\1/p' $pbndir/build.number` &&
 echo $version $rev &&
