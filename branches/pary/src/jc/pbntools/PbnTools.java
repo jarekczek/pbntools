54b25bbc4c3564b85e1ad6d766424b892d7723a9
jarekczek <jarekczek@cba49596-2f5b-4b15-a11e-bdda880d9e08>
2011-11-11 20:25:04 +0000

    OutputWindow at last working

--- branches/pary/src/jc/pbntools/PbnTools.java
+++ branches/pary/src/jc/pbntools/PbnTools.java
@@ -83,15 +83,8 @@ public class PbnTools {
   }
   
   static void pobierzPary(String sLink) {
-    // javax.swing.JOptionPane.showMessageDialog(null, "Pary!");
-    OutputWindow ow = OutputWindow.create(m_dlgMain, PbnTools.m_res);
-    ow.showWindow();
-    for (int i=1; i<=50; i++) { ow.addLine(""+i); }
-    javax.swing.JOptionPane.showMessageDialog(null, "ready!");
-    f.out("will close");
-    ow.close();
-
-    // ow.setVisible(true);
+    OutputWindow ow =  new OutputWindow(m_dlgMain, new PobierzPary(sLink), m_res);
+    ow.setVisible(true);
   }
     
   static void pobierzBbo(String sLink) {
