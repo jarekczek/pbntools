47c0e3653137b0fe0c7221d0fb6fca384cd18ea5
jarekczek <jarekczek@cba49596-2f5b-4b15-a11e-bdda880d9e08>
2012-06-17 20:07:36 +0000

    Updated HtmlTourDownloader api to allow duplicating deals with same hands and different results

--- trunk/src/jc/pbntools/download/DealReader.java
+++ trunk/src/jc/pbntools/download/DealReader.java
@@ -25,6 +25,6 @@ import jc.pbntools.Deal;
 
 public interface DealReader
 {
-  public Deal readDeal(String sUrl, boolean bSilent)
+  public Deal[] readDeals(String sUrl, boolean bSilent)
     throws HtmlTourDownloader.DownloadFailedException;
 }
