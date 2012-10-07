06ca83d1df8a7cf53313212a00796a71482d57af
Jarek Czekalski <jarekczek@poczta.onet.pl>
2012-10-07 17:00:06 +0000

    Made OutputWindow.Process class static

--- src/jc/pbntools/download/ParyTourDownloader.java
+++ src/jc/pbntools/download/ParyTourDownloader.java
@@ -232,7 +232,7 @@ public class ParyTourDownloader extends HtmlTourDownloader
       asCmdLine.set(0, sWget);
     }
     
-    OutputWindow.Process p = m_ow.createProcess();
+    OutputWindow.Process p = new OutputWindow.Process(m_ow);
     try {
       p.exec(asCmdLine.toArray(new String[0]));
     } catch (JCException e) {
